6d8cd3869bf0947f9a549ebf9c6b7ebfb513f1e8
Alexander <youdoit@mail.ru>
2020-11-27 02:21:45 +0600

    edited some of the html's

--- Content/urls.py
+++ Content/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import start
+from .views import start, start_2, start_3
 
 
 urlpatterns = [
-    path('', start)
+    path('', start),
+    path('start1', start_2),
+    path('start2', start_3)
 ]
--- Content/views.py
+++ Content/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def start_2(request):
+    return render(request, 'Content/start2.html')
+
+def start_3(request):
+    return render(request, 'Content/start3.html')
+
 def start(request):
     return render(request, 'Content/start.html')
