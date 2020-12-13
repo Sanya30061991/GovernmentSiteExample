65d960e5f2c6c7096fddd9ed7fd365784aa36e4e
Alexander <youdoit@mail.ru>
2020-12-14 00:41:27 +0600

    Created some descriptions to functions

--- Content/views.py
+++ Content/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, AnonymousUser
-from django.contrib.auth import login, authenticate, logout
 from .models import Citizen
 from .forms import CitReg, UserReg, UserLog, AvatarUpload
 
 # Create your views here.
+from .views_cases import auth, login_check, handle_image, log_off
+
 
 def log_ex(request):
-    logout(request)
-    return redirect("start")
+    return log_off(request)
+    
 
 def logon(request):
     context = {
@@ -16,56 +16,26 @@ def logon(request):
         'errors':""
     }
     if request.method == "POST":
-        email=request.POST['email']
-        password=request.POST['password']
-        user = authenticate(username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("main")
-        else:
-            context['errors'] = "Invalid login or password!"
+        return auth(request, context)
     return render(request, 'Content/login.html', context)
 
 def start(request):
-    a = 1/0
-    if request.user.is_authenticated:
-        context = {
-            'user':request.user
-        }
+    context = login_check(request)
     return render(request, 'Content/start.html', context)
 
-
 def main(request):
-    context = {
-        'user':None
-    }
-    if request.user.is_authenticated:
-        context = {
-            'user':request.user
-        }
+    context = login_check(request)
     return render(request, 'Content/main.html', context)
 
-def handle_uploaded_file(file, request):
-    with open(f'media/avatars/{request.user.email}.jpg', 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
-
 def profile(request):
+    context = login_check(request)
     context = {
-        'user':None
+        'cit':Citizen.objects.get(id=request.user.id),
+        'form':AvatarUpload(),
+        'test':5
     }
-    if request.user.is_authenticated:
-        context = {
-            'cit':Citizen.objects.get(id=request.user.id),
-            'form':AvatarUpload(),
-            'test':5
-        }
     if request.method == 'POST':
-        postfix = request.FILES['avatar'].name[request.FILES['avatar'].name.rfind("."):]
-        request.FILES['avatar'].name = request.user.email+postfix
-        handle_uploaded_file(request.FILES['avatar'], request)
-        context['cit'].avatar = request.FILES['avatar']
-        context['cit'].save()
+        handle_image(request, context)
     return render(request, 'Content/profile.html', context)
 
 def reg(request):
@@ -74,37 +44,5 @@ def reg(request):
         'form2':UserReg()
     }
     if request.method == "POST":
-        deps = {
-            '01':'Presidential branch',
-            '02':'Healthcare Sphere',
-            '03':'Educational Sphere',
-            '04':'Military Sphere',
-            '05':'Official'
-        }
-        ranks = {
-            '01':'Ordinary worker',
-            '02':'Manager',
-            '03':'Secretary',
-            '04':'Cleaner',
-            '05':'Overseer',
-            '06':'Main'
-        }
-        date_source = request.POST['birth_day']
-        date = date_source[-4:] + "-" + date_source[-7:-5] + "-" + date_source[-10:-8]
-        User.objects.create_user(
-            username = request.POST['email'],
-            first_name = request.POST['first_name'],
-            last_name = request.POST['last_name'],
-            email = request.POST['email'],
-            password = request.POST['password'],
-        )
-        new = Citizen(
-            user = User.objects.get(username = request.POST['email']),
-            department = deps.get(request.POST['res_code'][:2], "Other"),
-            rank = ranks.get(request.POST['res_code'][2:4], "Citizen"),
-            birth_day = date,
-            gender = request.POST['gender'],
-        )
-        new.save()
-        return redirect("login")
+        return user_creating(request, context)
     return render(request, 'Content/registration.html', context)
---
+++ Content/views_cases.py
@@ -0,0 +1,85 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
+from .models import Citizen
+
+def log_off(request):
+    """Simple logging-in method."""
+    logout(request)
+    return redirect("start")
+
+def auth(request, context):
+    """Authentication method, which consists of login and authenticate methods."""
+    email=request.POST['email']
+    password=request.POST['password']
+    user = authenticate(username=email, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect("main")
+    else:
+        context['errors'] = "Invalid login or password!"
+        return render(request, 'Content/login.html', context)
+
+
+def login_check(request):
+    """Method that checks whether user is logged in or not. Returns also a Citizen object if user is logged in."""
+    context = {
+        'user':None
+    }
+    if request.user.is_authenticated:
+        context = {
+            'user':request.user,
+            'cit':Citizen.objects.get(id=request.user.id)
+        }
+    return context
+
+def handle_uploaded_file(file, request):
+    """Function that handles uploaded file and writes it chunk by chunk in 'media/avatars/..."""
+    with open(f'media/avatars/{request.user.email}.jpg', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+def handle_image(request, context):
+    """Function that takes  image out of request.FILES and cuts off it's prefix and common name."""
+    postfix = request.FILES['avatar'].name[request.FILES['avatar'].name.rfind("."):]
+    request.FILES['avatar'].name = request.user.email+postfix
+    handle_uploaded_file(request.FILES['avatar'], request)
+    context['cit'].avatar = request.FILES['avatar']
+    context['cit'].save()
+
+def user_creating(request, context):
+    """Registration method, which considers user's job and status, and saving models."""
+    deps = {
+            '01':'Presidential branch',
+            '02':'Healthcare Sphere',
+            '03':'Educational Sphere',
+            '04':'Military Sphere',
+            '05':'Official'
+        }
+    ranks = {
+            '01':'Ordinary worker',
+            '02':'Manager',
+            '03':'Secretary',
+            '04':'Cleaner',
+            '05':'Overseer',
+            '06':'Main'
+        }
+    date_source = request.POST['birth_day']
+    date = date_source[-4:] + "-" + date_source[-7:-5] + "-" + date_source[-10:-8]
+    User.objects.create_user(
+        username = request.POST['email'],
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        password = request.POST['password'],
+    )
+    new = Citizen(
+        user = User.objects.get(username = request.POST['email']),
+        department = deps.get(request.POST['res_code'][:2], "Other"),
+        rank = ranks.get(request.POST['res_code'][2:4], "Citizen"),
+        birth_day = date,
+        gender = request.POST['gender'],
+    )
+    new.save()
+    return redirect("login")
