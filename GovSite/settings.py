02903d38dfc61daf48f3247339850c0ba08d8888
Sanya30061991 <61541621+Sanya30061991@users.noreply.github.com>
2020-12-26 01:20:43 +0600

    Update settings.py

--- GovSite/settings.py
+++ GovSite/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-cdn')
-MEDIA_URL = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media-cdn')
